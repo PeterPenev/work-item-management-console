5dcbe7bb4bb4167e38a6f783398843b7323108b6
PeterPenev <penev_peter@hotmail.com>
2019-01-28 16:38:28 +0200

    AddingBoardClass

---
+++ Wim/Wim.Models/Board.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Wim.Models.Interfaces;
+
+namespace Wim.Models
+{
+    class Board : IBoard
+    {
+        //fiels
+        private string name;
+        private List<WorkItem> workItems;
+        private List<ActivityHistory> activityHistory;
+
+        //constructor
+        public Board(string name)
+        {
+            this.name = name;
+        }
+
+        //properties
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public List<WorkItem> WorkItems
+        {
+            get
+            {
+                return new List<WorkItem>(this.workItems);
+            }
+        }
+
+        public List<ActivityHistory> ActivityHistory
+        {
+            get
+            {
+                return new List<ActivityHistory>(this.activityHistory);
+            }
+        }
+
+        //methods
+
+    }
+}
