b0e38849f702ea3658df8a8524b8ad5fdd408570
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-28 20:36:56 +0200

    AddingICosmeticsFactoryInterface

---
+++ Wim/Wim.Core/Contracts/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Core.Contracts
+{
+    interface ICommand
+    {
+        string Name { get; }
+
+        IList<string> Parameters { get; }
+    }
+}
---
+++ Wim/Wim.Core/Contracts/IWimFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Core.Contracts
+{
+    public interface IWimFactory
+    {
+        Team CreateTeam(string name);
+
+        Person CreatePerson(string name);
+
+        Member CreateMember(string name);
+
+        Board CreateMember(string name);
+    }
+}
