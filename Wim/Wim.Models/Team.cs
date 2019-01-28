42814f3f96ea543c2e5aa68bc98baadec62dd405
Vasil <vasilprodanov96@gmail.com>
2019-01-28 15:58:39 +0200

    AddingTeamClass

---
+++ Wim/Wim.Models/Team.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Models.Interfaces;
+
+namespace Wim.Models
+{
+    class Team : ITeam
+    {
+        private List<Board> boards;
+
+        private List<Member> members;
+
+        public Team(string name)
+        {
+            this.Name = name;
+            boards = new List<Board>();
+            members = new List<Member>();
+        }
+
+        public string Name { get; private set; }
+
+        List<Board> Boards
+        {
+            get
+            {
+                return new List<Board>(this.board);
+            }
+        }
+        List<Member> Members
+        {
+            get
+            {
+                return new List<Member>(this.member);
+            }
+        }
+    }
+}
