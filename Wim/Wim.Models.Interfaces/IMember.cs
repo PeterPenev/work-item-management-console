a546245f72fdc70ba58c746381bd2d05888654f2
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-28 14:35:51 +0200

    AddingITeamAndIMemberInterfaces

---
+++ Wim/Wim.Models.Interfaces/IMember.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Models.Interfaces
+{
+    public interface IMember
+    {
+        List<WorkItem> WorkItems { get; set; }
+        List<ActivityHistory> ActivityHistory { get; set; }
+    }
+}
---
+++ Wim/Wim.Models.Interfaces/ITeam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Models.Interfaces
+{
+    public interface ITeam
+    {
+        string Name { get; set; }
+        List<Board> Boards { get; set; }
+        List<Member> Members { get; set; }
+    }
+}
