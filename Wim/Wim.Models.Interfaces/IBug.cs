129972a6b6e543f74d369135670e393388554fb6
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-28 15:00:55 +0200

    AddingIBugInterface

---
+++ Wim/Wim.Models.Interfaces/IBug.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Models.Interfaces
+{
+    public interface IBug
+    {
+        Guid Id { get; set }
+        List<string> StepsToReproduce { get; set; }
+        Priority Priority { get; set; }
+        Severity Severity { get; set; }
+        Member Asignee { get; set; }
+    }
+}
