0fe366893b128f494e309318f6d44f9b7546c725
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-28 14:47:50 +0200

    AddingIBoardInterface

---
+++ Wim/Wim.Models.Interfaces/IBoard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Models.Interfaces
+{
+    public interface IBoard
+    {
+        string Name { get; set; }
+        List<WorkItem> WorkItems { get; set; }
+        List<ActivityHistory> ActivityHistory { get; set; }
+    }
+}
