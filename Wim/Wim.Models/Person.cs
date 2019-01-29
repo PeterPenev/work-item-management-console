c31369d26070c05955aff2c984883245f08ff977
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-29 17:51:53 +0200

    AddingPersonClass

--- Wim/Wim.Models/Person.cs
+++ Wim/Wim.Models/Person.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Wim.Models.Interfaces;
 
 namespace Wim.Models
 {
-    class Person
+    public class Person : IPerson
     {
+        private string name;
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("");
+                }
+
+                this.name = value;
+            }
+        }
+
     }
 }
