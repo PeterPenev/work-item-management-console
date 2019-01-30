3cd0850db6a9921d3c75bacfbe362985d716fd3a
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-30 16:38:41 +0200

    AddingBugSeverityEnum

---
+++ Wim/Wim.Models/Enumerations/BugSeverity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Models.Enumerations
+{
+    public enum BugSeverity
+    {
+        Critical,
+        Major,
+        Minor
+    }
+}
