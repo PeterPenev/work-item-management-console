d3fedf21591c9e2f2bbf3d3aa4aacb6364b73e73
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-01-31 14:09:42 +0200

    AddingIAllMembersAndIAllTeamsInterfaces

---
+++ Wim/Wim.Models.Interfaces/IAllMembers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wim.Models.Interfaces
+{
+    public interface IAllMembers
+    {
+        List<IMember> AllMembers { get; }
+    }
+}
