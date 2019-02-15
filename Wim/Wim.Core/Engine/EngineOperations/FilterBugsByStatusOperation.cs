211b7efd7906059e97eb3ceb9250010a7500473b
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:11:59 +0200

    FilterBugsByStatusOperation

---
+++ Wim/Wim.Core/Engine/EngineOperations/FilterBugsByStatusOperation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class FilterBugsByStatusOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public FilterBugsByStatusOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string FilterBugsByStatus(string statusToFilterBugFor)
+        {
+            //Validations
+            var statusTypeForChecking = "Status";
+            inputValidator.IsNullOrEmpty(statusToFilterBugFor, statusTypeForChecking);
+
+            inputValidator.ValidateIfAnyWorkItemsExist(allTeams);
+
+            inputValidator.ValidateIfAnyBugsExist(allTeams);
+
+            //Operations
+            var bugStatusToCheckFor = this.enumParser.GetBugStatus(statusToFilterBugFor);
+
+            var filteredBugsByStatus = allTeams.AllTeamsList.Values
+                .SelectMany(x => x.Boards)
+                    .SelectMany(x => x.WorkItems)
+                        .Where(x => x.GetType() == typeof(Bug))
+                            .Select(workItem => (Bug)workItem)
+                                .Where(bug => bug.BugStatus == bugStatusToCheckFor)
+                                    .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+            long workItemCounter = 1;
+
+            if (filteredBugsByStatus.Count == 0)
+            {
+                sb.AppendLine($"There are no Bugs with: {statusToFilterBugFor} Status in the app yet!");
+            }
+            else
+            {
+                sb.AppendLine($"----ALL BUGS WITH {statusToFilterBugFor} STATUS IN APPLICAITION----");
+                foreach (var item in filteredBugsByStatus)
+                {
+                    sb.AppendLine($"{workItemCounter}. {item.GetType().Name} with name: {item.Title} ");
+                    workItemCounter++;
+                }
+                sb.AppendLine("---------------------------------");
+            }
+
+            var resultedAllItems = sb.ToString().Trim();
+            return string.Format(resultedAllItems);
+        }
+
+    }
+}
