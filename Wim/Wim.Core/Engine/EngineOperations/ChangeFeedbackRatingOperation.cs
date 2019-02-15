99ef1f9ec1dfd84984c43ae5fa4ea68e1bea2958
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 20:42:01 +0200

    AddingChangeFeedbackOperations

---
+++ Wim/Wim.Core/Engine/EngineOperations/ChangeFeedbackRatingOperation.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ChangeFeedbackRatingOperation
+    {
+        private const string FeedbackRatingChanged = "Feedback {0} rating is changed to {1}";
+        private const string FeedbackStatusChanged = "Feedback {0} status is changed to {1}";
+
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ChangeFeedbackRatingOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ChangeFeedbackRating(string teamToChangeFeedbackRatingFor, string boardToChangeFeedbackRatingFor, string feedbackToChangeRatingFor, string newFeedbackRating, string authorOfFeedbackRatingChange)
+        {
+            //Validations
+            var feedbackTypeForChecking = "Feedback Title";
+            inputValidator.IsNullOrEmpty(feedbackToChangeRatingFor, feedbackTypeForChecking);
+
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToChangeFeedbackRatingFor, teamTypeForChecking);
+
+            var boardTypeForChecking = "Board Name";
+            inputValidator.IsNullOrEmpty(boardToChangeFeedbackRatingFor, boardTypeForChecking);
+
+            var ratingTypeForChecking = "Rating";
+            inputValidator.IsNullOrEmpty(newFeedbackRating, ratingTypeForChecking);
+
+            var authorTypeForChecking = "Author";
+            inputValidator.IsNullOrEmpty(authorOfFeedbackRatingChange, authorTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToChangeFeedbackRatingFor);
+
+            inputValidator.ValidateBoardExistanceInTeam(allTeams, boardToChangeFeedbackRatingFor, teamToChangeFeedbackRatingFor);
+
+            inputValidator.ValidateNoSuchFeedbackInBoard(allTeams, boardToChangeFeedbackRatingFor, teamToChangeFeedbackRatingFor, feedbackToChangeRatingFor);
+
+            var integerRating = inputValidator.ValidateRatingConversion(newFeedbackRating);
+
+            //Operations  
+            var itemType = "Feedback";
+
+            var castedFeedbackForRatingChange = allTeams.FindFeedbackAndCast(teamToChangeFeedbackRatingFor, boardToChangeFeedbackRatingFor, feedbackToChangeRatingFor);
+
+            castedFeedbackForRatingChange.ChangeFeedbackRating(integerRating);
+
+            var memberToAddActivityFor = allTeams.FindMemberInTeam(teamToChangeFeedbackRatingFor, authorOfFeedbackRatingChange);
+
+            var teamToAddActivityFor = allTeams.AllTeamsList[teamToChangeFeedbackRatingFor];
+
+            var feedbackToAddActivityFor = allTeams.FindWorkItem(teamToChangeFeedbackRatingFor, itemType, boardToChangeFeedbackRatingFor, feedbackToChangeRatingFor);
+
+            var teamToFindIn = allTeams.AllTeamsList[teamToChangeFeedbackRatingFor];
+
+            var boardToAddActivityFor = allTeams.FindBoardInTeam(teamToChangeFeedbackRatingFor, boardToChangeFeedbackRatingFor);
+
+            boardToAddActivityFor.AddActivityHistoryToBoard(memberToAddActivityFor, feedbackToAddActivityFor, newFeedbackRating);
+
+            memberToAddActivityFor.AddActivityHistoryToMember(feedbackToAddActivityFor, teamToFindIn, boardToAddActivityFor, newFeedbackRating);
+
+            feedbackToAddActivityFor.AddActivityHistoryToWorkItem(memberToAddActivityFor, feedbackToAddActivityFor, newFeedbackRating);
+
+            return string.Format(FeedbackRatingChanged, feedbackToChangeRatingFor, integerRating);
+        }
+
+    }
+}
---
+++ Wim/Wim.Core/Engine/EngineOperations/ChangeFeedbackStatusOperation.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ChangeFeedbackStatusOperation
+    {
+        private const string FeedbackStatusChanged = "Feedback {0} status is changed to {1}";
+
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ChangeFeedbackStatusOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ChangeFeedbackStatus(string teamToChangeFeedbackStatusFor, string boardToChangeFeedbackStatusFor, string feedbackToChangeStatusFor, string newFeedbackStatus, string authorOfFeedbackStatusChange)
+        {
+            //Validations
+            var feedbackTypeForChecking = "Feedback Title";
+            inputValidator.IsNullOrEmpty(feedbackToChangeStatusFor, feedbackTypeForChecking);
+
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToChangeFeedbackStatusFor, teamTypeForChecking);
+
+            var boardTypeForChecking = "Board Name";
+            inputValidator.IsNullOrEmpty(boardToChangeFeedbackStatusFor, boardTypeForChecking);
+
+            var statusTypeForChecking = "Status";
+            inputValidator.IsNullOrEmpty(newFeedbackStatus, statusTypeForChecking);
+
+            var authorTypeForChecking = "Author";
+            inputValidator.IsNullOrEmpty(authorOfFeedbackStatusChange, authorTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToChangeFeedbackStatusFor);
+
+            inputValidator.ValidateBoardExistanceInTeam(allTeams, boardToChangeFeedbackStatusFor, teamToChangeFeedbackStatusFor);
+
+            inputValidator.ValidateNoSuchFeedbackInBoard(allTeams, boardToChangeFeedbackStatusFor, teamToChangeFeedbackStatusFor, feedbackToChangeStatusFor);
+
+            //Operations
+            var itemType = "Feedback";
+
+            var newStatusEnum = enumParser.GetFeedbackStatus(newFeedbackStatus);
+
+            var castedFeedbackForStatusChange = allTeams.FindFeedbackAndCast(teamToChangeFeedbackStatusFor, boardToChangeFeedbackStatusFor, feedbackToChangeStatusFor);
+
+            castedFeedbackForStatusChange.ChangeFeedbackStatus(newStatusEnum);
+
+            var memberToAddActivityFor = allTeams.FindMemberInTeam(teamToChangeFeedbackStatusFor, authorOfFeedbackStatusChange);
+
+            var teamToAddActivityFor = allTeams.AllTeamsList[teamToChangeFeedbackStatusFor];
+
+            var feedbackToAddActivityFor = allTeams.FindWorkItem(teamToChangeFeedbackStatusFor, itemType, boardToChangeFeedbackStatusFor, feedbackToChangeStatusFor);
+
+            var teamToFindIn = allTeams.AllTeamsList[teamToChangeFeedbackStatusFor];
+
+            var boardToAddActivityFor = allTeams.FindBoardInTeam(teamToChangeFeedbackStatusFor, boardToChangeFeedbackStatusFor);
+
+            boardToAddActivityFor.AddActivityHistoryToBoard(memberToAddActivityFor, feedbackToAddActivityFor, newFeedbackStatus);
+
+            memberToAddActivityFor.AddActivityHistoryToMember(feedbackToAddActivityFor, teamToFindIn, boardToAddActivityFor, newFeedbackStatus);
+
+            feedbackToAddActivityFor.AddActivityHistoryToWorkItem(memberToAddActivityFor, feedbackToAddActivityFor, newFeedbackStatus);
+
+            return string.Format(FeedbackStatusChanged, feedbackToChangeStatusFor, newStatusEnum);
+        }
+    }
+}
