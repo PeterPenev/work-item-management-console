5ae7fe10ab173e221b0ae916a6a419295061ebf7
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:08:55 +0200

    AddingFilterBugsByPriorityOperation

---
+++ Wim/Wim.Core/Engine/EngineOperations/FilterBugsByPriorityOperation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class FilterBugsByPriorityOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public FilterBugsByPriorityOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string FilterBugsByPriority(string priorityToFilterBugFor)
+        {
+            //Validations
+            var priorityTypeForChecking = "Priority";
+            inputValidator.IsNullOrEmpty(priorityToFilterBugFor, priorityTypeForChecking);
+
+            inputValidator.ValidateIfAnyWorkItemsExist(allTeams);
+
+            inputValidator.ValidateIfAnyBugsExist(allTeams);
+
+            //Operations
+            var priorityToCheckFor = this.enumParser.GetPriority(priorityToFilterBugFor);
+
+            var filteredBugsByPriority = allTeams.AllTeamsList.Values
+                .SelectMany(x => x.Boards)
+                    .SelectMany(x => x.WorkItems)
+                        .Where(x => x.GetType() == typeof(Bug))
+                            .Select(workItem => (Bug)workItem)
+                                .Where(bug => bug.Priority == priorityToCheckFor)
+                                    .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+
+            long workItemCounter = 1;
+
+            if (filteredBugsByPriority.Count == 0)
+            {
+                sb.AppendLine($"There are no Bugs with: {priorityToFilterBugFor} Priority in the app yet!");
+            }
+            else
+            {
+                sb.AppendLine($"----ALL BUGS WITH {priorityToFilterBugFor} PRIORITY IN APPLICAITION----");
+                foreach (var item in filteredBugsByPriority)
+                {
+                    sb.AppendLine($"{workItemCounter}. {item.GetType().Name} with name: {item.Title} ");
+                    workItemCounter++;
+                }
+                sb.AppendLine("---------------------------------");
+            }
+
+            var resultedAllItems = sb.ToString().Trim();
+            return string.Format(resultedAllItems);
+        }
+    }
+}
