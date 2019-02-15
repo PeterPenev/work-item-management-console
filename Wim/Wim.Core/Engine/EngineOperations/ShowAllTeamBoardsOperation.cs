8f7cb157695eef22c4f21f1f27688db678fb70e7
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:27:48 +0200

    AddingShowAllTeamBoardsAndMembersOperations

---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowAllTeamBoardsOperation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowAllTeamBoardsOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowAllTeamBoardsOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ShowAllTeamBoards(string teamToShowBoardsFor)
+        {
+            //Validations
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToShowBoardsFor, teamTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToShowBoardsFor);
+
+            inputValidator.ValdateIfBoardsExistInTeam(allTeams, teamToShowBoardsFor);
+
+            //Operations
+            var allTeamBoardsResult = allTeams.AllTeamsList[teamToShowBoardsFor].ShowAllTeamBoards();
+            return string.Format(allTeamBoardsResult);
+        }
+    }
+}
---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowAllTeamMembersOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowAllTeamMembersOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowAllTeamMembersOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ShowAllTeamMembers(string teamToShowMembersFor)
+        {
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToShowMembersFor, teamTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToShowMembersFor);
+
+            var allTeamMembersStringResult = allTeams.AllTeamsList[teamToShowMembersFor].ShowAllTeamMembers();
+            return string.Format(allTeamMembersStringResult);
+        }
+    }
+}
