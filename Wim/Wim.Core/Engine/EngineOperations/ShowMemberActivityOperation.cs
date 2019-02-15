99f5bc00a36cdb5e268e67e7dae3866ce6c93519
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:23:05 +0200

    AddingShowAllActivityOperations

---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowMemberActivityOperation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowMemberActivityOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowMemberActivityOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ShowMemberActivityToString(string memberName)
+        {
+            //Validations
+            var inputTypeForChecking = "Member Name";
+            inputValidator.IsNullOrEmpty(memberName, inputTypeForChecking);
+
+            inputValidator.ValidateMemberExistance(allMembers, memberName);
+
+            //Operations
+            var selectedMember = this.allMembers.AllMembersList[memberName];
+            var memberActivities = selectedMember.ShowMemberActivityToString();
+
+            return string.Format(memberActivities);
+        }
+    }
+}
