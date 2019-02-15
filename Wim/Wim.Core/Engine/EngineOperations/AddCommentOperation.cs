ffdffaed25c563599b80cccae52896f8d8f6f401
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 20:59:39 +0200

    AddingAddCommentOperation

---
+++ Wim/Wim.Core/Engine/EngineOperations/AddCommentOperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class AddCommentOperation
+    {
+        private const string AddedCommentFor = "Comment {0} with author {1} is added to {2} with name: {3}.";
+
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+        private readonly IWimFactory factory;
+
+        public AddCommentOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser,
+            IWimFactory factory)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+            this.factory = factory;
+        }
+
+        public string AddComment(string teamToAddCommentToWorkItemFor, string boardToAddCommentToWorkItemFor, string itemTypeToAddWorkItemFor, string workitemToAddCommentFor, string authorOfComment, string commentToAdd)
+        {
+            //Validations
+            var itemTypeForChecking = "Item Title";
+            inputValidator.IsNullOrEmpty(workitemToAddCommentFor, itemTypeForChecking);
+
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToAddCommentToWorkItemFor, teamTypeForChecking);
+
+            var boardTypeForChecking = "Board Name";
+            inputValidator.IsNullOrEmpty(boardToAddCommentToWorkItemFor, boardTypeForChecking);
+
+            var authorTypeForChecking = "Author";
+            inputValidator.IsNullOrEmpty(authorOfComment, authorTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToAddCommentToWorkItemFor);
+
+            inputValidator.ValidateBoardExistanceInTeam(allTeams, boardToAddCommentToWorkItemFor, teamToAddCommentToWorkItemFor);
+
+            inputValidator.ValidateIfAnyWorkItemsExist(allTeams);
+
+            inputValidator.ValidateItemExistanceInBoard(allTeams, boardToAddCommentToWorkItemFor, teamToAddCommentToWorkItemFor, workitemToAddCommentFor);
+
+            //Operations
+            var workItemToAddCommentTo = allTeams.FindWorkItem(teamToAddCommentToWorkItemFor, itemTypeToAddWorkItemFor, boardToAddCommentToWorkItemFor, workitemToAddCommentFor);
+
+            workItemToAddCommentTo.AddComment(commentToAdd, authorOfComment);
+
+            return string.Format(AddedCommentFor, commentToAdd, authorOfComment, itemTypeToAddWorkItemFor, workitemToAddCommentFor);
+        }
+    }
+}
