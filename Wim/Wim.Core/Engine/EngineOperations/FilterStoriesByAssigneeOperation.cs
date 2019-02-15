4f658b8208579be66f516bbe5c825332a6591122
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:15:42 +0200

    AddingFilterStoriesByAssigneeOperation

---
+++ Wim/Wim.Core/Engine/EngineOperations/FilterStoriesByAssigneeOperation.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class FilterStoriesByAssigneeOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public FilterStoriesByAssigneeOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string FilterStoriesByAssignee(string assigneeToFilterStoryFor)
+        {
+            //Validations
+            var assigneeTypeForChecking = "Assignee";
+            inputValidator.IsNullOrEmpty(assigneeToFilterStoryFor, assigneeTypeForChecking);
+
+            inputValidator.ValidateIfAnyWorkItemsExist(allTeams);
+
+            inputValidator.ValidateIfAnyStoriesExist(allTeams);
+
+            //Operations
+            var filteredStoriesByAssignee = allTeams.AllTeamsList.Values
+                .SelectMany(x => x.Boards)
+                    .SelectMany(x => x.WorkItems)
+                        .Where(x => x.GetType() == typeof(Story))
+                            .Select(workItem => (Story)workItem)
+                                .Where(story => story.Assignee.Name == assigneeToFilterStoryFor)
+                                    .ToList();
+
+
+            StringBuilder sb = new StringBuilder();
+            long workItemCounter = 1;
+
+            if (filteredStoriesByAssignee.Count == 0)
+            {
+                sb.AppendLine($"There are no Stories with: {assigneeToFilterStoryFor} Assignee in the app yet!");
+            }
+            else
+            {
+                sb.AppendLine($"----ALL STORIES ASSIGNED TO MEMBER: {assigneeToFilterStoryFor} IN APPLICAITION----");
+                foreach (var item in filteredStoriesByAssignee)
+                {
+                    sb.AppendLine($"{workItemCounter}. {item.GetType().Name} with name: {item.Title} ");
+                    workItemCounter++;
+                }
+                sb.AppendLine("---------------------------------");
+            }
+
+            var resultedAllItems = sb.ToString().Trim();
+            return string.Format(resultedAllItems);
+        }
+    }
+}
