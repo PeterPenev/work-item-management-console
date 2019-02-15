9841aee886a2a8ccacdb1ceeaba48151ff7da4ad
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 20:43:48 +0200

    AddingAssignUnassignItemOperation

---
+++ Wim/Wim.Core/Engine/EngineOperations/AssignUnassignItemOperation.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class AssignUnassignItemOperation
+    {
+        private const string AssignItemTo = "{0} with name: {1} on board {2} part of team {3} was assigned to member {4}!";
+
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public AssignUnassignItemOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+        public string AssignUnassignItem(string teamToAssignUnsignItem, string boardToAssignUnsignItem, string itemType, string itemToAssignUnsign, string memberToAssignItem)
+        {
+            //Validations
+            var itemTypeForChecking = "Item Title";
+            inputValidator.IsNullOrEmpty(itemToAssignUnsign, itemTypeForChecking);
+
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToAssignUnsignItem, teamTypeForChecking);
+
+            var boardTypeForChecking = "Board Name";
+            inputValidator.IsNullOrEmpty(boardToAssignUnsignItem, boardTypeForChecking);
+
+            var authorTypeForChecking = "Author";
+            inputValidator.IsNullOrEmpty(memberToAssignItem, authorTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToAssignUnsignItem);
+
+            inputValidator.ValidateMemberExistance(allMembers, memberToAssignItem);
+
+            inputValidator.ValidateBoardExistanceInTeam(allTeams, boardToAssignUnsignItem, teamToAssignUnsignItem);
+
+            //Operations
+            var itemMemberToAssign = allTeams.FindMemberInTeam(teamToAssignUnsignItem, memberToAssignItem);
+
+            var itemToChangeIn = allTeams.FindWorkItem(teamToAssignUnsignItem, itemType, boardToAssignUnsignItem, itemToAssignUnsign);
+
+            IMember itemMemberBeforeUnssign = null;
+
+            if (itemType == "Bug")
+            {
+                var typedItem = (Bug)itemToChangeIn;
+
+                itemMemberBeforeUnssign = typedItem.Assignee;
+
+                typedItem.AssignMemberToBug(itemMemberToAssign);
+
+                itemMemberBeforeUnssign.RemoveWorkItemIdToMember(typedItem.Id);
+
+                itemMemberToAssign.AddWorkItemIdToMember(typedItem.Id);
+            }
+            else if (itemType == "Story")
+            {
+                var typedItem = (Story)itemToChangeIn;
+
+                itemMemberBeforeUnssign = typedItem.Assignee;
+
+                typedItem.AssignMemberToStory(itemMemberToAssign);
+
+                itemMemberBeforeUnssign.RemoveWorkItemIdToMember(typedItem.Id);
+
+                itemMemberToAssign.AddWorkItemIdToMember(typedItem.Id);
+            }
+
+            //history
+            var indexOfBoardInSelectedTeam = allTeams.AllTeamsList[teamToAssignUnsignItem].Boards.FindIndex(boardIndex => boardIndex.Name == boardToAssignUnsignItem);
+
+            //add history to board
+            allTeams.AllTeamsList[teamToAssignUnsignItem].Boards[indexOfBoardInSelectedTeam].AddActivityHistoryAfterAssignUnsignToBoard(itemType, itemToAssignUnsign, itemMemberToAssign, itemMemberBeforeUnssign);
+
+            //add history to member before unssign
+            itemMemberBeforeUnssign.AddActivityHistoryAfterUnsignToMember(itemType, itemToAssignUnsign, itemMemberBeforeUnssign);
+
+            //add history to member after assign
+            itemMemberToAssign.AddActivityHistoryAfterAssignToMember(itemType, itemToAssignUnsign, itemMemberToAssign);
+
+            return string.Format(AssignItemTo, itemType, itemToAssignUnsign, boardToAssignUnsignItem, teamToAssignUnsignItem, memberToAssignItem);
+        }
+    }
+}
