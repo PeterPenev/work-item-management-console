d199b83e9061fea71e2ec3d20f255a22f693b2bc
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:29:04 +0200

    AddingListAllWorkItemsOperation

---
+++ Wim/Wim.Core/Engine/EngineOperations/ListAllWorkItemsOperation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ListAllWorkItemsOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ListAllWorkItemsOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ListAllWorkItems()
+        {
+            //Validations
+            inputValidator.ValidateIfAnyWorkItemsExist(allTeams);
+
+            //Operations
+            var AllWorkItems = allTeams.AllTeamsList.Values
+                .SelectMany(x => x.Boards)
+                    .SelectMany(x => x.WorkItems)
+                        .ToList();
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("----ALL WORK ITEMS IN APPLICAITION----");
+            long workItemCounter = 1;
+            foreach (var item in AllWorkItems)
+            {
+                sb.AppendLine($"{workItemCounter}. {item.GetType().Name} with name: {item.Title} ");
+                workItemCounter++;
+            }
+            sb.AppendLine("---------------------------------");
+
+            var resultedAllItems = sb.ToString().Trim();
+            return string.Format(resultedAllItems);
+        }
+    }
+}
