e1d46775f33cd929ebaa5f9155ab5bee45f8ff17
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:25:48 +0200

    AddingShowMemberAndBoardActivityOperations

---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowBoardActivityOperation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowBoardActivityOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowBoardActivityOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ShowBoardActivity(string teamToShowBoardActivityFor, string boardActivityToShow)
+        {
+            //Validations
+            var teamTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamToShowBoardActivityFor, teamTypeForChecking);
+
+            var boardTypeForChecking = "Board Name";
+            inputValidator.IsNullOrEmpty(boardActivityToShow, boardTypeForChecking);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamToShowBoardActivityFor);
+
+            inputValidator.ValidateBoardExistanceInTeam(allTeams, boardActivityToShow, teamToShowBoardActivityFor);
+
+            //Operations
+            var boardToDisplayActivityFor = allTeams.AllTeamsList[teamToShowBoardActivityFor].Boards
+              .Where(boardInSelectedTeam => boardInSelectedTeam.Name == boardActivityToShow)
+                .FirstOrDefault();
+
+            var boardActivityToString = boardToDisplayActivityFor.ShowBoardActivityToString();
+            return string.Format(boardActivityToString);
+        }
+    }
+}
---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowTeamActivityOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowTeamActivityOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowTeamActivityOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ShowTeamActivity(string teamName)
+        {
+            //Validations
+            var inputTypeForChecking = "Team Name";
+            inputValidator.IsNullOrEmpty(teamName, inputTypeForChecking);
+
+            inputValidator.ValdateIfAnyTeamsExist(allTeams);
+
+            inputValidator.ValidateTeamExistance(allTeams, teamName);
+
+            //Operations
+            var teamToCheckHistoryFor = allTeams.AllTeamsList[teamName];
+            var teamActivityHistory = teamToCheckHistoryFor.ShowTeamActivityToString();
+
+            return string.Format(teamActivityHistory);
+        }
+    }
+}
