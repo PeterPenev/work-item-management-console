e9cda3db258ea258d6a73039192a28ef63edbca3
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-15 21:21:44 +0200

    AddingShowAllOperations

---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowAllPeopleOperation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowAllPeopleOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowAllPeopleOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+
+        public string ShowAllPeople()
+        {
+            //Validations
+            inputValidator.ValdateIfAnyMembersExist(allMembers);
+
+            //Operations
+            var peopleToDisplay = allMembers.ShowAllMembersToString();
+
+            return string.Format(peopleToDisplay);
+        }
+    }
+}
---
+++ Wim/Wim.Core/Engine/EngineOperations/ShowAllTeamsOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Core.Contracts;
+using Wim.Models.Interfaces;
+
+namespace Wim.Core.Engine.EngineOperations
+{
+    public class ShowAllTeamsOperation
+    {
+        private readonly IInputValidator inputValidator;
+        private readonly IAllTeams allTeams;
+        private readonly IAllMembers allMembers;
+        private readonly IEnumParser enumParser;
+
+        public ShowAllTeamsOperation(
+            IInputValidator inputValidator,
+            IAllTeams allTeams,
+            IAllMembers allMembers,
+            IEnumParser enumParser)
+        {
+            this.inputValidator = inputValidator;
+            this.allTeams = allTeams;
+            this.allMembers = allMembers;
+            this.enumParser = enumParser;
+        }
+        public string ShowAllTeams()
+        {
+            //Validations
+            inputValidator.ValdateIfAnyTeamsExist(allTeams);
+
+            //Operations
+            var teamsToDisplay = allTeams.ShowAllTeamsToString();
+
+            return string.Format(teamsToDisplay);
+        }
+    }
+}
