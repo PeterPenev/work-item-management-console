7e8c82c28a890127b95731a5ecdc90a00b0458cb
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-20 16:21:39 +0200

    AddingBugShouldAssignPriorityAndSeverityTests

--- Wim/Wim.UnitTests/Wim.Models.Tests/Bug_Should.cs
+++ Wim/Wim.UnitTests/Wim.Models.Tests/Bug_Should.cs
@@ -12,6 +12,7 @@ namespace Wim.UnitTests.Wim.Models.Tests
     [TestClass]
     public class Bug_Should
     {
+        [TestMethod]
         public void Constructor_Should_AssignsCorrectTitle()
         {
             //Arrange
@@ -27,5 +28,43 @@ namespace Wim.UnitTests.Wim.Models.Tests
             //Assert
             Assert.AreEqual(sut.Title, title);
         }
+
+        [TestMethod]
+        public void Constructor_Should_AssignsCorrectPriority()
+        {
+            //Arrange
+            var factory = new WimFactory();
+            var title = "ExampleBug";
+            var priorityToAssign = Priority.Low;
+            var mockPerson = new Mock<IMember>();
+            IList<string> stepsToReproduce = new List<string>() { "1. ExampleStepOne", "2.ExampleStepTwo" };
+            var descritpion = "Example Description";
+
+            //Act
+            var sut = factory.CreateBug(title, priorityToAssign, Severity.Minor, mockPerson.Object, stepsToReproduce, descritpion);
+
+            //Assert
+            Assert.AreEqual(sut.Priority, priorityToAssign);
+        }
+
+        [TestMethod]
+        public void Constructor_Should_AssignsCorrectSeverity()
+        {
+            //Arrange
+            var factory = new WimFactory();
+            var title = "ExampleBug";
+            var severityToAssign = Severity.Minor;
+            var mockPerson = new Mock<IMember>();
+            IList<string> stepsToReproduce = new List<string>() { "1. ExampleStepOne", "2.ExampleStepTwo" };
+            var descritpion = "Example Description";
+
+            //Act
+            var sut = factory.CreateBug(title, Priority.Low, severityToAssign, mockPerson.Object, stepsToReproduce, descritpion);
+
+            //Assert
+            Assert.AreEqual(sut.Severity, severityToAssign);
+        }
     }
+
+
 }
