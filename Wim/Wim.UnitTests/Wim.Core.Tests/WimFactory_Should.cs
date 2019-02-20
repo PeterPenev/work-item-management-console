94757ce428d004b8e3020ec3f18b1be431b388d8
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-20 16:13:29 +0200

    AddingCreateModelsUnitTests

--- Wim/Wim.UnitTests/Wim.Core.Tests/WimFactory_Should.cs
+++ Wim/Wim.UnitTests/Wim.Core.Tests/WimFactory_Should.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Wim.Core.Engine;
+using Wim.Models;
+using Wim.Models.Enums;
+using Wim.Models.Interfaces;
 
 namespace Wim.UnitTests.Wim.Core.Tests
 {
@@ -12,10 +16,65 @@ namespace Wim.UnitTests.Wim.Core.Tests
         [TestMethod]
         public void CreateBug_Should_ReturnCorrectInstance()
         {
-            //Assert
+            //Arrange
             var factory = new WimFactory();
+            var title = "ExampleBug";
+            var mockPerson = new Mock<IMember>();
+            IList<string> stepsToReproduce = new List<string>() { "1. ExampleStepOne", "2.ExampleStepTwo" };
+            var descritpion = "Example Description";
 
-            //var sut = factory.CreateBug()
+            //Act
+            var sut = factory.CreateBug(title, Priority.High, Severity.Minor, mockPerson.Object, stepsToReproduce, descritpion);
+            
+            //Assert
+            Assert.IsInstanceOfType(sut, typeof(Bug));
+        }
+
+        [TestMethod]
+        public void CreateStory_Should_ReturnCorrectInstance()
+        {
+            //Arrange
+            var factory = new WimFactory();
+            var title = "ExampleStory";
+            var mockPerson = new Mock<IMember>();
+            var descritpion = "Example Description";
+
+            //Act
+            var sut = factory.CreateStory(title, descritpion, Priority.High, Size.Large, StoryStatus.InProgress, mockPerson.Object);
+
+            //Assert
+            Assert.IsInstanceOfType(sut, typeof(Story));
+        }
+
+
+        [TestMethod]
+        public void CreateFeedback_Should_ReturnCorrectInstance()
+        {
+            //Arrange
+            var factory = new WimFactory();
+            var title = "ExampleFeedback";
+            var rating = 4;
+            var descritpion = "Example Description";
+
+            //Act
+            var sut = factory.CreateFeedback(title, descritpion, rating, FeedbackStatus.New);
+
+            //Assert
+            Assert.IsInstanceOfType(sut, typeof(Feedback));
+        }
+
+        [TestMethod]
+        public void CreateBoard_Should_ReturnCorrectInstance()
+        {
+            //Arrange
+            var factory = new WimFactory();
+            var title = "ExampleBoard";
+
+            //Act
+            var sut = factory.CreateBoard(title);
+
+            //Assert
+            Assert.IsInstanceOfType(sut, typeof(Board));
         }
     }
 }
