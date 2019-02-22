86b8389d1ebb6db7d9147e775a71251e4b5f3532
Vasil <vasilprodanov96@gmail.com>
2019-02-22 16:01:52 +0200

    AddingBusinessValidatorTest

--- Wim/Wim.UnitTests/Wim.Core.Tests/BusinessLogicValidator_Should.cs
+++ Wim/Wim.UnitTests/Wim.Core.Tests/BusinessLogicValidator_Should.cs
@@ -41,5 +41,30 @@ namespace Wim.UnitTests.Wim.Core.Tests
             //Assert
             Assert.AreEqual(ex.Message, "There are no Teams in the Application yet!");
         }
+
+        [TestMethod]
+        public void ValidateIfBoardExistsInTeam_Should_ThrowException()
+        {
+            //Arange
+            var mockAllTeams = new Mock<IAllTeams>();
+            mockAllTeams.Setup(x => x.AllTeamsList).Returns(new Dictionary<string,ITeam>());
+            var mockTeam = new Mock<ITeam>();
+            var mockBoard = new Mock<IBoard>();
+            mockBoard.Setup(x => x.Name).Returns("Board01");
+            mockTeam.Setup(x => x.Name).Returns("Alpha");
+
+            mockTeam.Setup(x => x.Boards).Returns(new List<IBoard>());
+            mockTeam.Object.Boards.Add(mockBoard.Object);
+            mockAllTeams.Object.AllTeamsList.Add("Alpha", mockTeam.Object);
+            
+
+            var businesLogicValidator = new BusinessLogicValidator();
+
+            //Act
+            var ex = Assert.ThrowsException<BoardAlreadyExistsInTeamException>(() => businesLogicValidator.ValidateBoardAlreadyInTeam(mockAllTeams.Object, mockBoard.Object.Name, mockTeam.Object.Name));
+
+            //Assert
+            Assert.AreEqual(ex.Message, "Board with name Board01 already exists in team Alpha!");
+        }
     }
 }
