8769b522e58d3ac5933b332110da7ec5b39724b8
PeterPenev <penev_peter@hotmail.com>
2019-02-22 14:49:27 +0200

    AddUnitTestConstructorStory_ShouldAssignCorrectTitle

---
+++ Wim/Wim.UnitTests/Wim.Models.Tests/Story_Should.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Wim.Core.Engine;
+using Wim.Models.Enums;
+using Wim.Models.Interfaces;
+
+namespace Wim.UnitTests.Wim.Models.Tests
+{
+    [TestClass]
+    public class Story_Should
+    {
+        [TestMethod]
+        public void Constructor_Should_AssignsCorrectTitle()
+        {
+            //Arrange
+            var factory = new WimFactory();
+
+            var storyTitle = "StoryTitleToCheck";
+            var storyDescription = "This is the first story description";
+            var storyPriority = Priority.High;
+            var storySize = Size.Large;
+            var storyStatus = StoryStatus.NotDone;
+            var MockStoryAssignee = new Mock<IMember>();
+
+            //Act
+            var sut = factory.CreateStory(storyTitle, storyDescription, storyPriority, storySize, storyStatus, MockStoryAssignee.Object);
+
+            //Assert
+            Assert.AreEqual(sut.Title, storyTitle);
+        }
+
+    }
+}
