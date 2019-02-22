5396caab1831b6a04125d5abba17b4a478153c0c
PeterPenev <penev_peter@hotmail.com>
2019-02-22 14:57:06 +0200

    AddUnitTestConstructorStory_ShouldAssignCorrectStatus

--- Wim/Wim.UnitTests/Wim.Models.Tests/Story_Should.cs
+++ Wim/Wim.UnitTests/Wim.Models.Tests/Story_Should.cs
@@ -69,5 +69,25 @@ namespace Wim.UnitTests.Wim.Models.Tests
             Assert.AreEqual(sut.Size, storySize);
         }
 
+        [TestMethod]
+        public void Constructor_Should_AssignsCorrectStatus()
+        {
+            //Arrange
+            var factory = new WimFactory();
+
+            var storyTitle = "StoryTitleToCheck";
+            var storyDescription = "This is the first story description";
+            var storyPriority = Priority.High;
+            var storySize = Size.Large;
+            var storyStatus = StoryStatus.NotDone;
+            var MockStoryAssignee = new Mock<IMember>();
+
+            //Act
+            var sut = factory.CreateStory(storyTitle, storyDescription, storyPriority, storySize, storyStatus, MockStoryAssignee.Object);
+
+            //Assert
+            Assert.AreEqual(sut.StoryStatus, storyStatus);
+        }
+
     }
 }
