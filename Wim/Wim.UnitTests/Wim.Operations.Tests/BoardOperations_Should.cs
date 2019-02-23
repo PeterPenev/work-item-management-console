3427e2936f0d14c8fe409480e720d633986e64b3
Vasil <vasilprodanov96@gmail.com>
2019-02-23 18:04:01 +0200

    AddingTestForAddWorkItemToBoard

---
+++ Wim/Wim.UnitTests/Wim.Operations.Tests/BoardOperations_Should.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Wim.Models.Interfaces;
+using Wim.Models.Operations;
+
+namespace Wim.UnitTests.Wim.Operations.Tests
+{
+    [TestClass]
+    public class BoardOperations_Should
+    {
+        [TestMethod]
+        public void Should_AddWorkItemToBoard()
+        {
+            //Arrange
+            var boardOperations = new BoardOperations();
+            var mockBoard = new Mock<IBoard>();
+            var mockWorkitem = new Mock<IBug>();
+            mockBoard.Setup(x => x.WorkItems).Returns(new List<IWorkItem>());
+
+            //Act
+            boardOperations.AddWorkitemToBoard(mockBoard.Object, mockWorkitem.Object);
+
+            //Assert
+            Assert.IsTrue(mockBoard.Object.WorkItems.Contains(mockWorkitem.Object));
+
+
+
+
+        }
+    }
+}
