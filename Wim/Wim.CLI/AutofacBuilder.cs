55c1235d019c0583d09b2fcb7579211e62bc395f
Dimitar Mihov <dimitar.mihov.bg@gmail.com>
2019-02-23 17:56:26 +0200

    RefactorAutoFacBuilder

--- Wim/Wim.CLI/AutofacBuilder.cs
+++ Wim/Wim.CLI/AutofacBuilder.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using Wim.Core.Contracts;
@@ -18,9 +19,11 @@ namespace Wim.CLI
         {
             var builder = new ContainerBuilder();
 
-            RegisterTypes(builder);
+            Assembly commandsAssembly = Assembly.GetAssembly(typeof(AddCommentOperation));
 
-            RegisterCommandsWithStrings(builder);
+            RegisterTypes(builder);           
+
+            RegisterCommandsWithStrings(builder, commandsAssembly);
 
             var container = builder.Build();
 
@@ -50,121 +53,17 @@ namespace Wim.CLI
                     .SingleInstance();
         }
 
-        public void RegisterCommandsWithStrings(ContainerBuilder builder)
+        public void RegisterCommandsWithStrings(ContainerBuilder builder, Assembly commandsAssembly)
         {
-            builder.RegisterType<AddCommentOperation>()
-               .Named<IEngineOperations>("AddComment");
+            var commandTypes = commandsAssembly.DefinedTypes
+               .Where(typeInfo => typeInfo.ImplementedInterfaces.Contains(typeof(IEngineOperations)))
+               .ToList();
 
-            builder.RegisterType<AddPersonToTeamOperation>()
-               .Named<IEngineOperations>("AddPersonToTeam");
-
-            builder.RegisterType<AssignUnassignItemOperation>()
-               .Named<IEngineOperations>("AssignUnassignItem");
-
-            builder.RegisterType<ChangeBugPriorityOperation>()
-               .Named<IEngineOperations>("ChangeBugPriority");
-
-            builder.RegisterType<ChangeBugSeverityOperation>()
-               .Named<IEngineOperations>("ChangeBugSeverity");
-
-            builder.RegisterType<ChangeBugStatusOperation>()
-                .Named<IEngineOperations>("ChangeBugStatus");
-
-            builder.RegisterType<ChangeFeedbackRatingOperation>()
-               .Named<IEngineOperations>("ChangeFeedbackRating");
-
-            builder.RegisterType<ChangeFeedbackStatusOperation>()
-               .Named<IEngineOperations>("ChangeFeedbackStatus");
-
-            builder.RegisterType<ChangeStoryPriorityOperation>()
-               .Named<IEngineOperations>("ChangeStoryPriority");
-
-            builder.RegisterType<ChangeStorySizeOperation>()
-               .Named<IEngineOperations>("ChangeStorySize");
-
-            builder.RegisterType<ChangeStoryStatusOperation>()
-               .Named<IEngineOperations>("ChangeStoryStatus");
-
-            builder.RegisterType<CreateBoardToTeamOperation>()
-               .Named<IEngineOperations>("CreateBoard");
-
-            builder.RegisterType<CreateBugOperation>()
-               .Named<IEngineOperations>("CreateBug");
-
-            builder.RegisterType<CreateFeedbackOperation>()
-               .Named<IEngineOperations>("CreateFeedback");
-
-            builder.RegisterType<CreatePersonOperation>()
-               .Named<IEngineOperations>("CreatePerson");
-
-            builder.RegisterType<CreateStoryOperation>()
-               .Named<IEngineOperations>("CreateStory");
-
-            builder.RegisterType<CreateTeamOperation>()
-               .Named<IEngineOperations>("CreateTeam");
-
-            builder.RegisterType<FilterBugsByAssigneeOperation>()
-               .Named<IEngineOperations>("FilterBugsByAssignee");
-
-            builder.RegisterType<FilterBugsByPriorityOperation>()
-               .Named<IEngineOperations>("FilterBugsByPriority");
-
-            builder.RegisterType<FilterBugsByStatusOperation>()
-               .Named<IEngineOperations>("FilterBugsByStatus");
-
-            builder.RegisterType<FilterBugsOperation>()
-               .Named<IEngineOperations>("FilterBugs");
-
-            builder.RegisterType<FilterFeedbacksByStatusOperation>()
-               .Named<IEngineOperations>("FilterFeedbacksByStatus");
-
-            builder.RegisterType<FilterFeedbacksOperation>()
-               .Named<IEngineOperations>("FilterFeedbacks");
-
-            builder.RegisterType<FilterStoriesByAssigneeOperation>()
-               .Named<IEngineOperations>("FilterStoriesByAssignee");
-
-            builder.RegisterType<FilterStoriesByPriorityOperation>()
-               .Named<IEngineOperations>("FilterStoriesByPriority");
-
-            builder.RegisterType<FilterStoriesByStatusOperation>()
-               .Named<IEngineOperations>("FilterStoriesByStatus");
-
-            builder.RegisterType<FilterStoriesOperation>()
-               .Named<IEngineOperations>("FilterStories");
-
-            builder.RegisterType<ListAllWorkItemsOperation>()
-               .Named<IEngineOperations>("ListAllWorkItems");
-
-            builder.RegisterType<ShowAllPeopleOperation>()
-               .Named<IEngineOperations>("ShowAllPeople");
-
-            builder.RegisterType<ShowAllTeamBoardsOperation>()
-               .Named<IEngineOperations>("ShowAllTeamBoards");
-
-            builder.RegisterType<ShowAllTeamMembersOperation>()
-               .Named<IEngineOperations>("ShowAllTeamMembers");
-
-            builder.RegisterType<ShowAllTeamsOperation>()
-               .Named<IEngineOperations>("ShowAllTeams");
-
-            builder.RegisterType<ShowBoardActivityOperation>()
-               .Named<IEngineOperations>("ShowBoardActivity");
-
-            builder.RegisterType<ShowMemberActivityOperation>()
-               .Named<IEngineOperations>("ShowPersonsActivity");
-
-            builder.RegisterType<ShowTeamActivityOperation>()
-               .Named<IEngineOperations>("ShowTeamsActivity");
-
-            builder.RegisterType<SortBugsByOperation>()
-               .Named<IEngineOperations>("SortBugsBy");
-
-            builder.RegisterType<SortFeedbacksByOperation>()
-               .Named<IEngineOperations>("SortFeedbacksBy");
-
-            builder.RegisterType<SortStoriesByOperation>()
-               .Named<IEngineOperations>("SortStoriesBy");
+            foreach (var commandType in commandTypes)
+            {
+                builder.RegisterType(commandType.AsType())
+                  .Named<IEngineOperations>(commandType.Name.Replace("Operation", ""));
+            }
         }
     }
 }
