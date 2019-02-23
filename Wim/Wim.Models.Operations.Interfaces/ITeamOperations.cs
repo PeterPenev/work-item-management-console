4d8702aa6d6d4ae162c9a0e10a0dc9a601fddc74
PeterPenev <penev_peter@hotmail.com>
2019-02-23 16:13:45 +0200

    GenerateTeamOperationClassChangeTeamInterfaceAndTeamClass

---
+++ Wim/Wim.Models.Operations.Interfaces/ITeamOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Wim.Models.Interfaces;
+
+namespace Wim.Models.Operations.Interfaces
+{
+    public interface ITeamOperations
+    {
+        void AddMember(ITeam team, IMember addToTeam);
+
+        void AddBoard(ITeam team, IBoard addToBoard);
+
+        string ShowAllTeamBoards(ITeam team);
+
+        string ShowAllTeamMembers(ITeam team);
+
+        string ShowTeamActivityToString(ITeam team);
+        
+    }
+}
--- Wim/Wim.Models/Team.cs
+++ Wim/Wim.Models/Team.cs
@@ -29,7 +29,7 @@ namespace Wim.Models
         {
             get
             {
-                return new List<IBoard>(this.boards);
+                return this.boards;
             }
         }
 
@@ -37,62 +37,62 @@ namespace Wim.Models
         {
             get
             {
-                return new List<IMember>(this.members);
+                return this.members;
             }
         }
 
-        public void AddMember(IMember addToTeam)
-        {
-            members.Add(addToTeam);
-        }
+        //public void AddMember(IMember addToTeam)
+        //{
+        //    members.Add(addToTeam);
+        //}
 
-        public void AddBoard(IBoard addToBoard)
-        {
-            boards.Add(addToBoard);
-        }
+        //public void AddBoard(IBoard addToBoard)
+        //{
+        //    boards.Add(addToBoard);
+        //}
 
-        //Methods
-        public string ShowAllTeamBoards()
-        {
-            int teamBoards = 1;
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Team name: {this.Name}");
+        ////Methods
+        //public string ShowAllTeamBoards()
+        //{
+        //    int teamBoards = 1;
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.AppendLine($"Team name: {this.Name}");
 
-            foreach (var selectedBoard in this.boards)
-            {
-                sb.AppendLine($"{teamBoards}. {selectedBoard.Name}");
-                teamBoards++;
-            }
+        //    foreach (var selectedBoard in this.boards)
+        //    {
+        //        sb.AppendLine($"{teamBoards}. {selectedBoard.Name}");
+        //        teamBoards++;
+        //    }
 
-            return sb.ToString().Trim();
-        }
+        //    return sb.ToString().Trim();
+        //}
 
-        public string ShowAllTeamMembers()
-        {
-            int teamMembers = 1;
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"Team name: {this.Name}");
+        //public string ShowAllTeamMembers()
+        //{
+        //    int teamMembers = 1;
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.AppendLine($"Team name: {this.Name}");
 
-            foreach(var selectedMember in this.members)
-            {
-                sb.AppendLine($"{teamMembers}. {selectedMember.Name}");
-                teamMembers++;
-            }
+        //    foreach(var selectedMember in this.members)
+        //    {
+        //        sb.AppendLine($"{teamMembers}. {selectedMember.Name}");
+        //        teamMembers++;
+        //    }
            
-            return sb.ToString().Trim();
-        }
+        //    return sb.ToString().Trim();
+        //}
 
-        public string ShowTeamActivityToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine($"==============TEAM: {this.Name}'s Activity History==============");
-            foreach (var member in this.Members)
-            {                
-                var memberActivityHistory = this.memberOpertaions.ShowMemberActivityToString(member);
-                sb.AppendLine(memberActivityHistory);                
-            }
-            sb.AppendLine($"****************End Of TEAM{this.Name}'s Activity History*****************");
-            return sb.ToString().Trim();
-        }
+        //public string ShowTeamActivityToString()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    sb.AppendLine($"==============TEAM: {this.Name}'s Activity History==============");
+        //    foreach (var member in this.Members)
+        //    {                
+        //        var memberActivityHistory = this.memberOpertaions.ShowMemberActivityToString(member);
+        //        sb.AppendLine(memberActivityHistory);                
+        //    }
+        //    sb.AppendLine($"****************End Of TEAM{this.Name}'s Activity History*****************");
+        //    return sb.ToString().Trim();
+        //}
     }
 }
