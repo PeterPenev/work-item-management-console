caff917d2561ec8f0b6d273fa7f3ffcb8e7a0752
PeterPenev <penev_peter@hotmail.com>
2019-02-25 16:50:33 +0200

    AddingMemberOperationsTestAddWorkItemIdToMember_ShouldAssignCorrectId

--- Wim/Wim.UnitTests/Wim.Operations.Tests/MemberOperations_Should.cs
+++ Wim/Wim.UnitTests/Wim.Operations.Tests/MemberOperations_Should.cs
@@ -28,5 +28,34 @@ namespace Wim.UnitTests.Wim.Operations.Tests
             Assert.AreEqual(guid, mockMember.Object.WorkItemsId[0]);
         }
 
+        [TestMethod]
+        public void AddActivityHistoryToMember_ShouldAddCorrectActivityHistory()
+        {
+            //Arrange
+            var memberOperations = new MemberOperations();
+            var mockMember = new Mock<IMember>();
+            mockMember.Setup(x => x.Name).Returns("Gosho");
+            mockMember.Setup(x => x.ActivityHistory).Returns(new List<IActivityHistory>());
+            var mockTrackedWorkItem = new Mock<IBug>();
+            mockTrackedWorkItem.Setup(x => x.Title).Returns("bug1234567");
+            var mockTrackedTeam = new Mock<ITeam>();
+            mockTrackedTeam.Setup(x => x.Name).Returns("Alpha");
+            var mockTrackedBoard = new Mock<IBoard>();
+            mockTrackedBoard.Setup(x => x.Name).Returns("TestBoard");
+
+            var strBuilder = new StringBuilder();
+            strBuilder.AppendLine($"Member: Gosho created: IBugProxy with Title: bug1234567 in Board: TestBoard part of Alpha Team!");
+
+            string resultToAddAssMessage = strBuilder.ToString().Trim();
+            var activityHistoryToAddToMember = mockMember.Object.ActivityHistory;
+
+            //Act
+            memberOperations.AddActivityHistoryToMember(mockMember.Object, mockTrackedWorkItem.Object, mockTrackedTeam.Object, mockTrackedBoard.Object);
+
+
+            //Assert
+            Assert.AreEqual(mockMember.Object.ActivityHistory[0].Message, resultToAddAssMessage);
+            
+        }
     }
 }
